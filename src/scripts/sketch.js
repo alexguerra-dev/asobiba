068c3fb3617dccd2635f7af55b957ef5d17e7990
Alex Guerra <alexander.a.guerra@gmail.com>
2025-05-01 23:59:59 -0500

    Refactor sketch.js to streamline image loading and enhance point rendering logic

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,49 +1,59 @@
-let image1, image2, image3, image4
-let images = []
+let imageOfLizard
 let font
+
 let points
-let tick = 0
 
 function preload() {
     font = loadFont('data/fonts/OpenSans-Regular.ttf')
-    image1 = loadImage('data/images/bobing.gif')
+    imageOfLizard = loadImage('data/images/bobing.gif')
 }
 
 function setup() {
+    // Setup the environment
     noLoop()
     rectMode(CENTER)
     angleMode(DEGREES)
+
+    // Create the canvas the size of the window.
     createCanvas(windowWidth, windowHeight)
 
-    points = font.textToPoints('Hello', 0, height / 2, 372, {
-        sampleFactor: 0.05,
+    // This makes a list of points from a font, text, and size.
+    points = font.textToPoints('Hello, World!', 0, height / 2, 372, {
+        sampleFactor: 0.031,
         simplifyThreshold: 0,
     })
 
+    // The total number of points representing the text.
     const numberOfPoints = points.length
 
     points.forEach((point, index, array) => {
-        // console.log(array)
         let x = point.x
         let y = point.y
-        // fill(255, 100, index)
 
         const aScaledValue = map(index, 0, numberOfPoints, 0, 360)
-        // console.log(sin(index))
-        // console.log(index)
-        // ellipse(x + 3 * sin(tick), y, 17, 15)
-        // ellipse(x + 9 * map(mouseX, 0, 900, 0, 1), y, 5, 5 * sin(tick))
-        // ellipse(x, y, 5, 10)
-        // ellipse(x, y, 5)
-        // fill(255, 200, 0)
-        // stroke(2
-        stroke(55, 0, 0)
-        ellipse(x, y, 15, 15)
-        strokeWeight(2)
-        stroke(200, 0, 0)
-        line(x, y, x + 3 * sin(aScaledValue), y + 3 * cos(aScaledValue))
+
+        stroke(255, 100, 170)
+        strokeWeight(3)
+        fill(100, 20)
+        ellipse(x, y, 35, 35)
+        fill(0)
+
+        if (index == 0) {
+            stroke(255, 100, 170)
+            strokeWeight(30)
+            fill(100, 20)
+            ellipse(x, y, 35, 35)
+        } else {
+            stroke(0)
+            strokeWeight(10)
+            fill(100, 20)
+            console.log(array[index - 1])
+            fill(200, 10)
+            line(array[index - 1].x, array[index - 1].y, x, y)
+        }
+        // line(x, y, x + 3 * sin(aScaledValue), y + 3 * cos(aScaledValue))
     })
-    // image1.loadPixels()
-    // image(image1, 0, 0, 200, 200)
-    // image(image1, 0, 100)
+    // Draw the image of the lizard
+    imageMode(CENTER)
+    image(imageOfLizard, width / 2, height / 2, 200, 200)
 }
