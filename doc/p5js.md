db9d56e18765f778931aedc118975c1cd3d6241c
Alex Guerra <alexander.a.guerra@gmail.com>
2025-05-02 10:17:25 -0500

    Refactor sketch.js to initialize points and enhance drawing logic with dynamic point rendering

--- doc/p5js.md
+++ doc/p5js.md
@@ -1,5 +1,18 @@
 # p5js Notes
 
+I can use `framerate()` to set the frame rate of the sketch. The default is 60 frames per second, but I can change it to 30 or any other value.
+
+```javascript
+function setup() {
+  createCanvas(400, 400)
+  frameRate(30) // Set the frame rate to 30 frames per second
+}
+function draw() {
+  background(220)
+  // Your drawing code here
+}
+```
+
 ---
 
 ## Links
--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,6 +1,7 @@
 // let imageOfLizard
 let font
 let tick
+let points
 
 function preload() {
     font = loadFont('data/fonts/OpenSans-Regular.ttf')
@@ -17,9 +18,38 @@ function setup() {
 
     // This makes a list of points from a font, text, and size.
     // The total number of points representing the text.
+    points = font.textToPoints(
+        `I think teresa is a pretty great person ${3}`,
+        0,
+        0,
+        16,
+        {
+            sampleFactor: 0.1,
+            simplifyThreshold: 0,
+        },
+    )
 }
 
 function draw() {
-    fill(0)
-    rect(100, 0, 200, 200)
+    background(255)
+    points.forEach((point) => {
+        // Draw each point as a circle
+        fill(255, 0, 0, 100)
+        // noStroke()
+        ellipse(point.x + 0.5 * mouseX, point.y + 0.5 * mouseY, 10, 10)
+    })
+    push()
+    translate(0.5 * width, 0.5 * height)
+    rotate(45)
+    fill(0, 10)
+    rect(100 + 0.1 * mouseX, 400 + 0.2 * mouseY, 400, 200)
+    translate(0.5 * width, 0.5 * height)
+    rotate(45)
+    rect(200 + 0.2 * mouseX, 400 + 0.2 * mouseY, 400, 200)
+    pop()
+    rect(200 + 0.2 * mouseX, 300 + 0.2 * mouseY, 400, 200)
+    rect(300, 400, 400, 200)
+    rect(400, 400, 800, 200)
+
+    // noLoop()
 }
