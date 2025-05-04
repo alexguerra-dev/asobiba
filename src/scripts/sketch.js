fb3f58b9f2f16a9138fe2565a796554978b41fbe
Alex Guerra <alexander.a.guerra@gmail.com>
2025-05-04 05:52:01 -0500

    Cool Cells

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -14,7 +14,7 @@ function setup() {
     textSize(32)
     textAlign(CENTER, CENTER)
 
-    setOfNodes = Array.from({ length: 10 }, (_, i) => {
+    setOfNodes = Array.from({ length: 15 }, (_, i) => {
         console.log(i)
         return new Node(random(width), random(height))
     })
@@ -25,7 +25,7 @@ class Node {
         this.x = x
         this.y = y
         this.size = 50
-        this.color = color(255)
+        // this.color = color(255)
         this.seed = random(1)
     }
 
@@ -35,11 +35,13 @@ class Node {
     }
     display() {
         this.update()
-        fill(this.color)
+        // fill(200)
+        // background(0)
         push()
-        noFill()
-        stroke(255)
-        strokeWeight(2)
+        fill(255, 100, 78, 100)
+        // noFill()
+        // stroke(255)
+        // strokeWeight(2)
         ellipse(this.x, this.y, this.size)
         ellipse(this.x, this.y, this.size * 0.5)
         pop()
@@ -48,12 +50,19 @@ class Node {
 
 function draw() {
     background(255, 100)
-    fill(255, 100)
+    // fill(0, 100)
     console.log(setOfNodes)
 
-    setOfNodes.forEach((node) => {
+    setOfNodes.forEach((node, index, array) => {
         node.display()
-        line(node.x, node.y, mouseX, mouseY)
+
+        // if (index == 0) {
+        //     line(node.x, node.y, mouseX, mouseY)
+        // }
+        // if (index === array.length - 1) {
+        //     line(node.x, node.y, array[0].x, array[0].y)
+        // }
+        // line(node.x, node.y, mouseX, mouseY)
     })
 
     // node1.display()
